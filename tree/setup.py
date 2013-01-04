54bc9586d183780d86ab5bf2721affbdff630aac
srinituraga <sturaga@gatsby.ucl.ac.uk>
2013-01-04 15:06:29 +0000

    fixed to be more basic

--- tree/setup.py
+++ tree/setup.py
@@ -1,23 +1,9 @@
-import os
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
 
-import numpy
-from numpy.distutils.misc_util import Configuration
-
-
-def configuration(parent_package="", top_path=None):
-    config = Configuration("tree", parent_package, top_path)
-    libraries = []
-    if os.name == 'posix':
-        libraries.append('m')
-    config.add_extension("_tree",
-                         sources=["_tree.c"],
-                         include_dirs=[numpy.get_include()],
-                         libraries=libraries)
-
-    config.add_subpackage("tests")
-
-    return config
-
-if __name__ == "__main__":
-    from numpy.distutils.core import setup
-    setup(**configuration().todict())
+setup(
+  name = 'sklearn decision tree',
+  cmdclass = {'build_ext': build_ext},
+  ext_modules = [Extension("_tree", ["_tree.pyx"])]
+)
